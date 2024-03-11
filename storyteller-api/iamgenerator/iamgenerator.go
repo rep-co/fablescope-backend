e3e79c2052ec374408c47cc7fa89ebd2b1ea3f9e
sheeiavellie <josephlandmars@gmail.com>
2024-03-11 18:24:25 +0300

    delete useless stuff

--- storyteller-api/iamgenerator/iamgenerator.go
+++ storyteller-api/iamgenerator/iamgenerator.go
@@ -2,7 +2,6 @@ package iamgenerator
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -15,8 +14,6 @@ type IAMTokenGenerator interface {
 }
 
 const (
-	protocol      = "https://"
-	apiGatewayURL = ".apigw.yandexcloud.net"
 	serverlessURL = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
 )
 
@@ -24,57 +21,12 @@ type IAMTokenHTTPResponse struct {
 	AccessToken string `json:"access_token"`
 }
 
-type IAMTokenHTTP struct {
-	client       *http.Client
-	apiGatewayID string
-}
-
-func NewIAMTokenHTTP(apiGatewayID string) *IAMTokenHTTP {
-	return &IAMTokenHTTP{
-		client:       &http.Client{},
-		apiGatewayID: apiGatewayID,
-	}
-}
-
-func (iam *IAMTokenHTTP) GenerateToken() (string, error) {
-	request, err := iam.newRequest()
-	if err != nil {
-		return "", err
-	}
-	response := IAMTokenHTTPResponse{}
-	err = iam.sendRequest(request, &response)
-	if err != nil {
-		return "", err
-	}
-
-	return response.AccessToken, nil
-}
-
-func (iam *IAMTokenHTTP) newRequest() (*http.Request, error) {
-	url := fmt.Sprintf("%s%s%s/iam", protocol, iam.apiGatewayID, apiGatewayURL)
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	return request, nil
-}
-
-func (iam *IAMTokenHTTP) sendRequest(request *http.Request, v any) error {
-	response, err := iam.client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	return json.NewDecoder(response.Body).Decode(v)
-}
-
 type IAMTokenServerless struct {
 	client *http.Client
 }
 
-func NewIAMTokenServerless() *IAMTokenHTTP {
-	return &IAMTokenHTTP{
+func NewIAMTokenServerless() *IAMTokenServerless {
+	return &IAMTokenServerless{
 		client: &http.Client{},
 	}
 }
