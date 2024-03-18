3962ccd7fd001a9ffb61f645f4e3d311192f4ab4
sheeiavellie <josephlandmars@gmail.com>
2024-03-19 02:15:59 +0300

    split account

--- wardrobe-auth/data/account.go
+++ wardrobe-auth/data/account.go
@@ -1,7 +1,16 @@
 package data
 
-type Account struct {
+import "github.com/google/uuid"
+
+type AccountCredentials struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type Account struct {
+	ID       uuid.UUID
+	Name     string
+	Email    string
+	Password string
 }
