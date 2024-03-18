34182ba6bb8005a3ac55351eebaec1b393ccbb3a
sheeiavellie <josephlandmars@gmail.com>
2024-03-19 01:54:22 +0300

    fix: raneme createuser to createaccount

--- wardrobe-auth/database/storage.go
+++ wardrobe-auth/database/storage.go
@@ -3,7 +3,7 @@ package database
 import "github.com/rep-co/fablescope-backend/wardrobe-auth/data"
 
 type Storage interface {
-	CreateUser(*data.Account) error
+	CreateAccount(*data.Account) error
 	GetAccount(*data.Account) error
 	UpdateAccount(*data.Account) error
 	DeleteAccount(*data.Account) error
