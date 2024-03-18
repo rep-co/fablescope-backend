ef9e1cb867bf61367f4c4e0bbccd151bbec149dc
sheeiavellie <josephlandmars@gmail.com>
2024-03-19 02:18:17 +0300

    fix: fix storage methods

--- wardrobe-auth/database/storage.go
+++ wardrobe-auth/database/storage.go
@@ -3,8 +3,8 @@ package database
 import "github.com/rep-co/fablescope-backend/wardrobe-auth/data"
 
 type Storage interface {
-	CreateAccount(*data.Account) error
-	GetAccount(*data.Account) error
-	UpdateAccount(*data.Account) error
-	DeleteAccount(*data.Account) error
+	CreateAccount(*data.AccountCredentials) error
+	GetAccount(*data.AccountCredentials) (*data.Account, error)
+	UpdateAccount(*data.AccountCredentials) (*data.Account, error)
+	DeleteAccount(*data.AccountCredentials) error
 }
