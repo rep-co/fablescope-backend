22d3b72daaf0788f79f82223542aac33e49eb55c
sheeiavellie <josephlandmars@gmail.com>
2024-03-19 04:10:53 +0300

    fix: update account

--- wardrobe-auth/data/account.go
+++ wardrobe-auth/data/account.go
@@ -9,8 +9,17 @@ type AccountRequest struct {
 }
 
 type Account struct {
-	ID           uuid.UUID
-	Name         string
-	Email        string
-	HashPassword string
+	ID       uuid.UUID
+	Name     string
+	Email    string
+	Password string
+}
+
+func NewAccount(request *AccountRequest) *Account {
+	return &Account{
+		ID:       uuid.New(),
+		Name:     request.Name,
+		Email:    request.Email,
+		Password: request.Password,
+	}
 }
