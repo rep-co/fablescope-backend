be6686199f32c1e64cc68e4dcb432e1d568465a1
sheeiavellie <josephlandmars@gmail.com>
2024-03-22 01:17:25 +0300

    feat: add binding to AccountRequest fields

--- wardrobe-auth/data/account.go
+++ wardrobe-auth/data/account.go
@@ -3,9 +3,9 @@ package data
 import "github.com/google/uuid"
 
 type AccountRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type Account struct {
