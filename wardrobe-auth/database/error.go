64d40eafed4aba4eeef42969a9b0489555c56223
sheeiavellie <josephlandmars@gmail.com>
2024-03-25 07:16:35 +0300

    feat: add executionerror

--- wardrobe-auth/database/error.go
+++ wardrobe-auth/database/error.go
@@ -5,6 +5,7 @@ import "fmt"
 var (
 	NoResultError    = storageError{Message: "no result error. Record doesn't exist"}
 	TransactionError = storageError{Message: "transaction error. Transaction rolled back"}
+	ExecutionError   = storageError{Message: "operation error. Operation can't be executed"}
 )
 
 type storageError struct {
