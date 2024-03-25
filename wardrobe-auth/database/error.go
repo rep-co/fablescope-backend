acecfd975fe04a98bf805b0d2473923013ca970f
sheeiavellie <josephlandmars@gmail.com>
2024-03-25 07:29:56 +0300

    feat: add requesttimeouterror

--- wardrobe-auth/database/error.go
+++ wardrobe-auth/database/error.go
@@ -3,9 +3,10 @@ package database
 import "fmt"
 
 var (
-	NoResultError    = storageError{Message: "no result error. Record doesn't exist"}
-	TransactionError = storageError{Message: "transaction error. Transaction rolled back"}
-	ExecutionError   = storageError{Message: "operation error. Operation can't be executed"}
+	NoResultError       = storageError{Message: "no result error. Record doesn't exist"}
+	TransactionError    = storageError{Message: "transaction error. Transaction rolled back"}
+	ExecutionError      = storageError{Message: "operation error. Operation can't be executed"}
+	RequestTimeoutError = storageError{Message: "request timeout"}
 )
 
 type storageError struct {
